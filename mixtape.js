e5e074ae891035fadf466ebdcc4a461332782ffe
shaina-espulgar <72699933+shaina-espulgar@users.noreply.github.com>
2023-12-31 20:19:12 +0800

    Update mixtape.js

--- mixtape.js
+++ mixtape.js
@@ -16,3 +16,20 @@ function audioVolume(amount) {
   let changevolume = document.getElementsByTagName("audio")[0];
   changevolume.volume = amount;
 }
+
+var modal = document.getElementById('myModal');
+
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
